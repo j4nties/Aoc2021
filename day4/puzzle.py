166f3597b8c00f4314686cb25d3b3de6006b2372
j4nties <110736778+j4nties@users.noreply.github.com>
2022-12-12 03:58:13 +0300

    Delete day4 directory

--- day4/puzzle.py
+++
@@ -1,7 +0,0 @@
-def get():
-    puzzleInput = []
-    with open('inputs.txt', 'r') as f:
-        lines = f.readlines()
-        puzzleInput = [line.strip() for line in lines]
-    return puzzleInput
-
